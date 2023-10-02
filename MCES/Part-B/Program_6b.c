53be5a71ee230a8b679f817174dbd6b52b2dd957
Gaurav Hegade <74713169+gauravhegade@users.noreply.github.com>
2023-10-02 17:46:39 +0530

    Update Program_6b.c

--- MCES/Part-B/Program_6b.c
+++ MCES/Part-B/Program_6b.c
@@ -29,7 +29,7 @@ void runDCMotor(int direction, int dutycycle)
 	PWMPCR = (1 << 14);					// enable PWM6
 	PWMMR0 = 1000;						// set PULSE rate to value suitable for DC Motor operation
 	PWMMR6 = (1000U * dutycycle) / 100; // set PULSE period
-	PWMTCR = 0x00000009;				// bit D3 = 1 (enable PWM), bit D0=1 (start the timer)
+	PWMTCR = 0x09;						// bit D3 = 1 (enable PWM), bit D0=1 (start the timer)
 	PWMLER = 0X70;						// load the new values to PWMMR0 and PWMMR6 registers
 }
 
