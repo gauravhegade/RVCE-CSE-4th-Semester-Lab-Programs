900135cae90e24d2cdf637be6ed3c3925939539c
Gaurav Hegade <neph005@protonmail.com>
2023-10-08 11:57:29 +0530

    Add Code Comments to N-Queens

--- DAA/8. N-Queens Problem.c
+++ DAA/8. N-Queens Problem.c
@@ -1,39 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int x[20];
-// k->row i->column
-int place(int k, int i)
-{
-    for (int j = 1; j <= k - 1; j++)
+
+// k = row
+// i = column
+int place(int k, int i) {
+    for (int j = 1; j < k; j++)
+        // if queens are in the same column or
+        // if queens are diagonal to each other
         if ((x[j] == i) || (abs(x[j] - i) == abs(j - k)))
             return 0;
     return 1;
 }
-void printQueen(int n)
-{
+
+void printQueen(int n) {
     static int count = 1;
-    printf("\n\nSolution %d is- \n\n", count++);
+
+    printf("\nSolution %d is: \n", count++);
+
     for (int i = 1; i <= n; ++i)
         printf("\t%d", i);
 
-    for (int i = 1; i <= n; ++i)
-    {
+    for (int i = 1; i <= n; ++i) {
         printf("\n%d", i);
-        for (int j = 1; j <= n; ++j) // for nxn board
-        {
+        for (int j = 1; j <= n; ++j) {
             if (x[i] == j)
-                printf("\tQ"); // queen at i,j position
+                printf("\tQ"); // place queen at position i, j
             else
-                printf("\t-"); // empty slot
+                printf("\t-");
         }
     }
+    printf("\n");
 }
-void NQueen(int k, int n)
-{
-    for (int i = 1; i <= n; i++)
-    {
-        if (place(k, i))
-        {
+
+// k = row number
+// i = column number
+void NQueen(int k, int n) {
+    for (int i = 1; i <= n; i++) {
+        if (place(k, i)) {
+            // place the queen kth row and ith column
             x[k] = i;
             if (k == n)
                 printQueen(n);
@@ -42,12 +48,11 @@ void NQueen(int k, int n)
         }
     }
 }
-int main()
-{
+
+int main() {
     int n;
 
-    printf("\nN-QUEEN PROBLEM\n");
-    printf("\nEnter the number of queens to be placed - ");
+    printf("\nEnter the number of queens to be placed: ");
     scanf("%d", &n);
 
     if (n == 2 || n == 3 || n == 0)
