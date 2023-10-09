3c99312405551412b583bc37a6d4f05aa53a52d8
Gaurav Hegade <neph005@protonmail.com>
2023-10-09 22:10:52 +0530

    Horspool now prints indices for multiple pattern occurrences

--- DAA/4. Horspool's String Matching.c
+++ DAA/4. Horspool's String Matching.c
@@ -1,58 +1,73 @@
 #include <stdio.h>
 #include <string.h>
 
-int table[1000], count;
+#define SIZE 1000
 
-void ShiftTableComputation(char p[]) {
+int table[SIZE];
+int indices[SIZE]; // Array to store the indices of pattern occurrences
+int j, i, shifts;
+
+void shiftTable(char p[]) {
     int m = strlen(p);
 
-    for (int i = 0; i < 1000; i++)
+    for (int i = 0; i < SIZE; i++)
         table[i] = m;
 
     for (int j = 0; j < m - 1; j++)
         table[p[j]] = m - 1 - j;
 }
 
-int HPoolStringMatching(char t[], char p[]) {
-    int m = strlen(p);
+void horspoolMatching(char t[], char p[]) {
     int n = strlen(t);
-    int i = m - 1, k;
+    int m = strlen(p);
 
-    // Shift Table Computation
-    ShiftTableComputation(p);
+    i = m - 1;
+    int k;
 
-    while (i <= n - 1) {
+    shiftTable(p);
+
+    while (i < n) {
         k = 0;
-        while ((k <= m - 1) && t[i - k] == p[m - 1 - k])
+
+        while (k < m && p[m - 1 - k] == t[i - k])
             k++;
 
-        if (k == m)
-            return i - m + 1;
-
-        else {
+        if (k == m) {
+            indices[j++] = i - m + 1;
             i += table[t[i]];
-            count++;
+            shifts++;
+        } else {
+            i += table[t[i]];
+            shifts++;
         }
     }
-    return -1;
 }
 
 int main() {
-    char t[100], p[100];
+    char p[SIZE], t[SIZE];
 
-    printf("Enter the text: ");
+    printf("Enter text: ");
     scanf("%[^\n]%*c", t);
 
     printf("Enter pattern: ");
     scanf("%[^\n]%*c", p);
 
-    int res = HPoolStringMatching(t, p);
+    j = 0;
 
-    if (res == -1)
-        printf("Not found");
+    horspoolMatching(t, p);
 
-    printf("Pattern found at position %d\n", res + 1);
-    printf("Number of shifts performed: %d\n", count);
+    if (j == 0) {
+        printf("Pattern not found in text");
+    } else {
+        printf("Pattern found at indices: ");
+        for (int i = 0; i < j; i++) {
+            printf("%d", indices[i]);
+            if (i < j - 1)
+                printf(", ");
+        }
+        printf("\nNumber of shifts performed are: %d\n", shifts);
+    }
 
+    printf("\n");
     return 0;
 }
